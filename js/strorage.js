42e6c38b903d2fe5b3aceb40d99fab758502bde5
Reyhan Tri R <reyhanramadan21@smk.belajar.id>
2023-05-19 20:49:10 +0700

    Fitur Pesan

---
+++ js/strorage.js
@@ -0,0 +1,45 @@
+// method
+function insert() {
+  const form = $("#formMessage").serializeArray();
+  let dataMessage = JSON.parse(localStorage.getItem("dataMessage")) || [];
+
+  let newData = {};
+  form.forEach(function (item, index) {
+    let name = item["name"];
+    let value = name === "id" || name === "" ? Number(item["value"]) : item["value"];
+    newData[name] = value;
+  });
+  // console.log(newData)
+  localStorage.setItem("dataMessage", JSON.stringify([...dataMessage, newData]));
+  return newData;
+}
+
+function showData(dataMessage) {
+  let row = "";
+
+  if (dataMessage.length == 0) {
+    return (row = `<h1 class="title" style="text-align : center">Belum Ada Pesan Masuk</h1>`);
+  }
+
+  dataMessage.forEach(function (item, index) {
+    row += `<h1 class="title">${item["nama"]}</h1>`;
+    row += `<h4>- ${item["hubungan"]}</h4>`;
+    row += `<p>${item["pesan"]}</p>`;
+  });
+  return row;
+}
+
+let dataMessage;
+$(function () {
+  // initialize
+  dataMessage = JSON.parse(localStorage.getItem("dataMessage")) || [];
+
+  $(".card-message").html(showData(dataMessage));
+  // events
+  $("#formMessage").on("submit", function (e) {
+    e.preventDefault();
+    dataMessage.push(insert());
+
+    $(".card-message").html(showData(dataMessage));
+  });
+});
