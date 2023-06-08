9f9d61e2deec27833443f832e06863a6911d3ae5
Reyhan Tri R <reyhanramadan21@smk.belajar.id>
2023-06-08 20:51:46 +0700

    compres image

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // fitur Hitung Mundur
 // Mengatur waktu akhir perhitungan mundur
-var countDownDate = new Date("Jun 30, 2023 10:00:00").getTime();
+var countDownDate = new Date("Jul 30, 2023 10:00:00").getTime();
 
 // Memperbarui hitungan mundur setiap 1 detik
 var x = setInterval(function () {
